b3d707cccaf063c7888b41c17cf49701cae3b565
Jon Jackson <ocskier@gmail.com>
2021-03-23 19:54:00 -0400

    refactor: convert all to classes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,3 @@
-const iconData = {
-  clear: {
-    day: 'assets/icons/iconfinder_weather-clear_118959.png',
-    night: 'assets/icons/iconfinder_weather-icons-night-clear_2087717.png',
-  },
-  clouds: {
-    day: 'assets/icons/iconfinder_icon-20-clouds_316271.png',
-  },
-  rain: {
-    day: 'assets/icons/iconfinder_weather-showers-scattered_118964.png',
-  },
-  thunderstorm: {
-    day: 'assets/icons/iconfinder_thunderstorm-ranny_1221016.png',
-  },
-  snow: {
-    day: 'assets/icons/iconfinder_cloud_snow_367530.png',
-  },
-  mist: {
-    day: 'assets/icons/iconfinder_weather_30_2682821.png',
-  },
-  drizzle: {
-    day: 'assets/icons/iconfinder_weather-showers-scattered_118964.png',
-  },
-};
-
-let searchedList = JSON.parse(localStorage.getItem('searchedList')) || [];
-let lastSearch = localStorage.getItem('lastSearch') || 'Raleigh';
-
 const searchUrl =
   'https://api.openweathermap.org/data/2.5/weather?APPID=833a5451ef3423413fe6e789b8687cf3&units=imperial';
 const forecastUrl =
@@ -36,107 +8,80 @@ const forecastUrl =
 // iconUrl += 'client_secret=gV3NcGeUtZlQIpY1peUeGNUJy70dJaojzJ66TBnBiEahY5pPR8xaDiluftnR19Mt&';
 // iconUrl += 'count=1&premium=0&query=';
 
-init();
-
-$('#searchIcon').click(() => {
-  let status = $('form').css('visibility');
-  $('form').attr(
-    'style',
-    `visibility: ${status === 'visible' ? 'hidden' : 'visible'}`
-  );
-});
-
-$('form').submit(e => {
-  e.preventDefault();
-  lastSearch = $('input')
-    .val()
-    .trim();
-  $('input').val('');
-  lastSearch && runSearch(lastSearch);
-});
-
-$('.collection').on('click', '.searchListItem', function() {
-  lastSearch = $(this).text();
-  lastSearch && runSearch(lastSearch);
-});
-
-function init() {
-  searchedList && printSearchedCities();
-  runSearch(lastSearch);
-}
-
-function printSearchedCities() {
-  let collection = $('.collection');
-  collection.empty();
-  collection.attr('style', 'visibility: visible');
-  for (let i = 0; i < searchedList.length; i++) {
-    collection.append(
-      `<li class="collection-item searchListItem">${searchedList[i].toUpperCase()}</li>`
-    );
-  }
-}
-
 function buildUrl(baseUrl, extendedUrl) {
   return baseUrl + extendedUrl;
 }
 
-async function runSearch(searchTerm) {
-  for (el in ['h2', '#weatherDesc']) {
-    $(el).text('');
-  }
-  $('#weatherDetails').empty();
-  $('#currentIcon img').remove();
-
-  try {
-    let resp = await $.get(buildUrl(searchUrl, `&q=${searchTerm}`));
-    resp &&
-      searchedList.indexOf(searchTerm.toLowerCase()) === -1 &&
-      searchedList.push(searchTerm.toLowerCase()) &&
-      printSearchedCities();
-    resp && localStorage.setItem('lastSearch', searchTerm.toLowerCase());
-    resp && localStorage.setItem('searchedList', JSON.stringify(searchedList));
-    resp && printTodaysForecast(resp);
-    resp && printExtendedForecast(searchTerm,resp.coord.lat,resp.coord.lon);
-  } catch (err) {
-    console.log(err);
-  }
+class OWAPI {
+  getWeather = (searchTerm) => $.get(buildUrl(searchUrl, `&q=${searchTerm}`));
+  getForecast = (lat, lon) =>
+    $.get(buildUrl(forecastUrl, `&lat=${lat}&lon=${lon}`));
 }
-
-function printTodaysForecast(data) {
-  $('h2').text(data.name.toUpperCase());
-  let imgHTML = `<img width=300 height=200 src=${
-    iconData[data.weather[0].main.toLowerCase()].day
-  } alt="Current Icon">`;
-  $('#currentIcon').append(imgHTML);
-  $('#weatherDesc').text(
-    data.weather[0].description[0].toUpperCase() +
-      data.weather[0].description.slice(1)
-  );
-  $('#weatherDetails').append(
-    `<span style="font-size:3.2rem;padding-bottom:12px">${Math.round(
-      data.main.temp_max
-    )}°F</span>
+class Screen {
+  constructor() {
+    this.searchedList = JSON.parse(localStorage.getItem('searchedList')) || [];
+    this.lastSearch = localStorage.getItem('lastSearch') || 'Raleigh';
+    this.searchEl = $('#searchIcon');
+    this.form = $('form');
+    this.cityInputEl = $('input');
+    this.cityCollectionEl = $('.collection');
+    this.weatherDetailsEl = $('#weatherDetails');
+  }
+  getInputCity = (e) => {
+    e.preventDefault();
+    const lastSearch = this.cityInputEl.val().trim();
+    this.cityInputEl.val('');
+    return lastSearch;
+  };
+  hideSearch = () => {
+    this.form.attr(
+      'style',
+      `visibility: ${
+        this.form.css('visibility') === 'visible' ? 'hidden' : 'visible'
+      }`
+    );
+  };
+  setLastSearch = (city) => {
+    this.lastSearch = city;
+  };
+  printSearchedCities = () => {
+    this.cityCollectionEl.empty();
+    this.cityCollectionEl.attr('style', 'visibility: visible');
+    this.searchedList.forEach((city) => {
+      this.cityCollectionEl.append(
+        `<li class="collection-item searchListItem">${city.toUpperCase()}</li>`
+      );
+    });
+  };
+  printTodaysForecast = (data) => {
+    $('h2').text(data.name.toUpperCase());
+    let imgHTML = `<img width=300 height=200 src=${
+      iconData[data.weather[0].main.toLowerCase()].day
+    } alt="Current Icon">`;
+    $('#currentIcon').append(imgHTML);
+    $('#weatherDesc').text(
+      data.weather[0].description[0].toUpperCase() +
+        data.weather[0].description.slice(1)
+    );
+    $('#weatherDetails').append(
+      `<span style="font-size:3.2rem;padding-bottom:12px">${Math.round(
+        data.main.temp_max
+      )}°F</span>
       <div style="display: flex;flex-direction: column;justify-content: space-around;">
       <span>Wind Chill: ${Math.round(data.main.feels_like)}°F</span>
       <span>Wind: ${Math.floor(data.wind.speed)} mph</span>
       <span>Humidity: ${data.main.humidity}%</span>
       </div>`
-  );
-  $('#todaysForecast').attr('style', 'visibility: visible');
-}
+    );
+    $('#todaysForecast').attr('style', 'visibility: visible');
+  };
+  printExtendedForecast = (data) => {
+    $('#extendedForecast').empty();
 
-async function printExtendedForecast(searchTerm,lat,lon) {
-  $('#extendedForecast').empty();
-
-  try {
-    let resp = await $.get(buildUrl(forecastUrl, `&lat=${lat}&lon=${lon}`));
-    // old code for the 40 3hr increments of forecast data
-    // let today = new Date();
-    // const filteredForecastList = resp.list
-    //   .filter(item => new Date(item.dt_txt).getDate() != today.getDate())
-    //   .filter(item => new Date(item.dt_txt).getHours() === 12);
-    const filteredForecastList = resp.daily;
-    $('#weatherDetails').find('div').append(`<span>UV Index: ${filteredForecastList[0].uvi}</span>`);
+    const filteredForecastList = data.daily;
+    $('#weatherDetails')
+      .find('div')
+      .append(`<span>UV Index: ${filteredForecastList[0].uvi}</span>`);
     let col1 = $('<div class="col s12 l1"></div>');
     $('#extendedForecast').append(col1);
 
@@ -170,9 +115,7 @@ async function printExtendedForecast(searchTerm,lat,lon) {
         .attr('height', 130)
         .attr('style', 'width: auto !important; margin: 0 auto');
       cardSpan.addClass('card-title extendedTitle');
-      cardSpan.text(
-        new Date(filteredForecastList[i].dt*1000).toDateString()
-      );
+      cardSpan.text(new Date(filteredForecastList[i].dt * 1000).toDateString());
       cardImgDiv.append(cardSpan, cardImg);
 
       cardContent.addClass('card-content center extendedContent');
@@ -180,8 +123,7 @@ async function printExtendedForecast(searchTerm,lat,lon) {
       contentP.attr('style', 'font-size:1.1rem;');
       let weather = filteredForecastList[i].weather[0];
       contentP.text(
-        weather.description[0].toUpperCase() +
-        weather.description.slice(1)
+        weather.description[0].toUpperCase() + weather.description.slice(1)
       );
       contentDetails.addClass('extendedDetails').html(
         `<span style="font-size:2.2rem">${Math.round(
@@ -196,7 +138,7 @@ async function printExtendedForecast(searchTerm,lat,lon) {
       contentPrecip.text(
         `Precip: ${
           filteredForecastList[i]['rain']
-            ? (filteredForecastList[i].rain/25.4).toFixed(2)
+            ? (filteredForecastList[i].rain / 25.4).toFixed(2)
             : 0
         }`
       );
@@ -216,7 +158,80 @@ async function printExtendedForecast(searchTerm,lat,lon) {
 
     let col6 = $('<div class="col s12 l1"></div>');
     $('#extendedForecast').append(col6);
-  } catch (err) {
-    console.log(err);
-  }
+  };
 }
+
+class App {
+  constructor() {
+    this.API = new OWAPI();
+    this.screen = new Screen();
+  }
+
+  runSearch = async () => {
+    try {
+      const weatherData = await this.API.getWeather(this.screen.lastSearch);
+      // for (el in ['h2', '#weatherDesc']) {
+      //   $(el).text('');
+      // }
+      this.screen.weatherDetailsEl.empty();
+      $('#currentIcon img').remove();
+      weatherData &&
+        this.screen.searchedList.indexOf(
+          this.screen.lastSearch.toLowerCase()
+        ) === -1 &&
+        this.screen.searchedList.push(this.screen.lastSearch.toLowerCase()) &&
+        this.screen.printSearchedCities();
+      weatherData &&
+        localStorage.setItem(
+          'lastSearch',
+          this.screen.lastSearch.toLowerCase()
+        );
+      weatherData &&
+        localStorage.setItem(
+          'searchedList',
+          JSON.stringify(this.screen.searchedList)
+        );
+      weatherData && this.screen.printTodaysForecast(weatherData);
+      const forecastData = await this.API.getForecast(
+        weatherData.coord.lat,
+        weatherData.coord.lon
+      );
+      this.screen.printExtendedForecast(forecastData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+}
+
+const WeatherDashboard = new App();
+
+WeatherDashboard.screen.searchEl.click(WeatherDashboard.screen.hideSearch);
+
+WeatherDashboard.screen.form.submit(function (e) {
+  const input = WeatherDashboard.screen.getInputCity(e);
+  WeatherDashboard.screen.setLastSearch(input);
+  WeatherDashboard.runSearch();
+});
+
+WeatherDashboard.screen.cityCollectionEl.on(
+  'click',
+  '.searchListItem',
+  function () {
+    let input = $(this).text().trim();
+    input && WeatherDashboard.screen.setLastSearch(input);
+    input && WeatherDashboard.runSearch();
+  }
+);
+
+WeatherDashboard.screen.searchedList &&
+  WeatherDashboard.screen.printSearchedCities();
+WeatherDashboard.runSearch();
+
+// data &&
+//   searchedList.indexOf(searchTerm.toLowerCase()) === -1 &&
+//   searchedList.push(searchTerm.toLowerCase()) &&
+//   printSearchedCities();
+// resp && localStorage.setItem('lastSearch', searchTerm.toLowerCase());
+// resp && localStorage.setItem('searchedList', JSON.stringify(searchedList));
+// resp && printTodaysForecast(resp);
+// resp && printExtendedForecast(searchTerm, resp.coord.lat, resp.coord.lon);
---
+++ assets/js/icons.js
@@ -0,0 +1,24 @@
+const iconData = {
+  clear: {
+    day: 'assets/icons/iconfinder_weather-clear_118959.png',
+    night: 'assets/icons/iconfinder_weather-icons-night-clear_2087717.png',
+  },
+  clouds: {
+    day: 'assets/icons/iconfinder_icon-20-clouds_316271.png',
+  },
+  rain: {
+    day: 'assets/icons/iconfinder_weather-showers-scattered_118964.png',
+  },
+  thunderstorm: {
+    day: 'assets/icons/iconfinder_thunderstorm-ranny_1221016.png',
+  },
+  snow: {
+    day: 'assets/icons/iconfinder_cloud_snow_367530.png',
+  },
+  mist: {
+    day: 'assets/icons/iconfinder_weather_30_2682821.png',
+  },
+  drizzle: {
+    day: 'assets/icons/iconfinder_weather-showers-scattered_118964.png',
+  },
+};
