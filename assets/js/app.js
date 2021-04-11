3d1ff31f77087f2712cfbcd2ce8ebc9dada18d13
Jon Jackson <36890724+ocskier@users.noreply.github.com>
2021-04-10 23:13:48 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,3 @@
-const searchUrl =
-  'https://api.openweathermap.org/data/2.5/weather?APPID=833a5451ef3423413fe6e789b8687cf3&units=imperial';
-const forecastUrl =
-  'https://api.openweathermap.org/data/2.5/onecall?APPID=833a5451ef3423413fe6e789b8687cf3&units=imperial&exclude=hourly,minutely';
-
 // let iconUrl = 'https://api.iconfinder.com/v3/icons/search?';
 // iconUrl += 'client_id=mXK8DHYcamId9eZ9lMHRipV2zSMQdn3oPi1GkmT12Pq4FmKtTPiNqfmIxsgNb93W&';
 // iconUrl += 'client_secret=gV3NcGeUtZlQIpY1peUeGNUJy70dJaojzJ66TBnBiEahY5pPR8xaDiluftnR19Mt&';
@@ -13,9 +8,13 @@ function buildUrl(baseUrl, extendedUrl) {
 }
 
 class OWAPI {
-  getWeather = (searchTerm) => $.get(buildUrl(searchUrl, `&q=${searchTerm}`));
+  constructor() {
+    this.searchUrl = 'https://api.openweathermap.org/data/2.5/weather?APPID=833a5451ef3423413fe6e789b8687cf3&units=imperial';
+    this.forecastUrl = 'https://api.openweathermap.org/data/2.5/onecall?APPID=833a5451ef3423413fe6e789b8687cf3&units=imperial&exclude=hourly,minutely';
+  }
+  getWeather = (searchTerm) => $.get(buildUrl(this.searchUrl, `&q=${searchTerm}`));
   getForecast = (lat, lon) =>
-    $.get(buildUrl(forecastUrl, `&lat=${lat}&lon=${lon}`));
+    $.get(buildUrl(this.forecastUrl, `&lat=${lat}&lon=${lon}`));
   runSearch = (city) => {
     const search = async () => {
       try {
